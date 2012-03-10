f544046db33c056667f2f2bd4c4dba6983e3ae60
David Bruant <bruant.d@gmail.com>
2012-03-10 13:20:52 +0100

    virage avant de tracer les polygones moi-même

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
-"use strict";
 
 (function(){
-    var map;
+    "use strict";
+    var DOUBLE_QUOTE = '"';
+    
+    String.prototype.unquote = function(){
+        if(this[0] === DOUBLE_QUOTE && this[this.length-1] === DOUBLE_QUOTE){
+            return this.substring(1, this.length-1);
+        }
+        if(this[0] !== DOUBLE_QUOTE && this[this.length-1] !== DOUBLE_QUOTE){
+            return this;
+        }
+        
+        console.warn("unhandled case", this);
+    };
+
+})();
+
+
+(function(){
+    "use strict";
+    var geomap;
+    var data = Object.create(null); // TODO shim (+forEach, + map)
 
     $(function initialize() {
         var myOptions = {
@@ -9,23 +28,71 @@
             zoom: 12,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-        map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+        geomap = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
     });
     
     $(function(){
         var kmllayer = new google.maps.KmlLayer('http://davidbruant.github.com/OpenDataLab2012/BDVBDX.kml', {
-            map: map,
+            map: geomap,
+            //suppressInfoWindows: true | false, // Suppress the rendering of info windows when layer features are clicked.
             clickable: true
         });
+        
         google.maps.event.addListener(kmllayer, 'status_changed', function(){
             console.log('status changed event');
             console.log('new status', kmllayer.getStatus());
+            //console.log(Object.keys(kmllayer));
         })
 
-
         console.log('kmllayer status', kmllayer.getStatus());
+    });
+    
+    var csvDataP = $.ajax({
+        url:'./Législatives bordeaux.csv',
+        dataType: "text"
+    });
+    csvDataP.fail(function(csvError){
+        console.log('CSV Error', csvError);
+    });
         
-        //kmllayer.setMap(map);
-    })
+    csvDataP.then(function(csvData){
+        console.log('CSV retrieved');
+        // parsing CSV
+        var lines = csvData.split('\n');
+        lines.forEach(function(l, i){
+            lines[i] = lines[i].split(';');
+
+            // removing freaking quote
+            lines[i].forEach(function(e, j){
+                lines[i][j] = e.unquote() || lines[i][j];
+            });
+        });
+        
+        var firstLine = lines.shift();
+        
+        console.log(firstLine);
+        console.log(lines.slice(0,3));
+        
+        var dataArray = lines.map(function(l){
+            var d = {};
+            
+            l.forEach(function(val, i){
+                d[firstLine[i]] = val;
+            });
+            
+            return d;
+        });
+        
+        console.log(dataArray);
+        
+        dataArray.forEach(function(e){
+            var key = e.section + ' ' + e['Bur.'];
+        
+        })
+        
+    });
+
+    
+    
 })();
 
